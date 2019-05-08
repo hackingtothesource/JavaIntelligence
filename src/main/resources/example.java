1470ce6c59a5a126d76e7fa9a0d82af4dc13707a
PragmaTwice <e.d.boy@aliyun.com>
2019-05-08 09:05:58 +0800

    resource: update example source

--- src/main/resources/example.java
+++ src/main/resources/example.java
@@ -1,7 +1,7 @@
 package
         Graph;
 
-import java.util.LinkedList;
+import java. util .LinkedList;
 
 
 public class  Graph {
@@ -9,43 +9,44 @@ public class  Graph {
     private final   int V;private int E ;
              private LinkedList<Integer>[] adj;
 
-    public Graph(int V)
+    public Graph(int     V)
     {
         this.V=V;
         this.E=0;
         adj =    (LinkedList  <Integer>[  ]) new LinkedList[   V];
         for (int v =0; v<    V; v  ++)
-            adj[v] = new  LinkedList<>();
+            adj[v  ] = new  LinkedList<  >();
     }
 
-    public int V(    )
-    {
+        public int V(    )
+        {
 
 
 
-        return V;
-    }
+            return V;
+        }
 
     public
     int E() {
-            return  E;
+                return  E;
     }
 
     public void addEdge(int v, int w) {
-        adj[v].add( w);
+           adj[v].add( w);
         adj[w ].add(v );
         E ++ ;
     }
 
     public LinkedList<  Integer> adj(int v) {
-        return adj[v];
+        return adj[v]
+                ;
     }
 
     public    int degree(  int v,Graph g)    {
-        int count = 0;
-        for(int s : adj(v)  )
-            count++;
-            return count;
+        int count =   0;
+        for(int s :   adj(v)  )
+            count++ ;
+            return  count;
         }
 
 }
